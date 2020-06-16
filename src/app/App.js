02f70e7f7e34886a821f0cf5a9f26ca7ee6f5d06
Omaru Jawara <omaru.jawara94@gmail.com>
2020-06-16 15:14:22 -0400

    Login page created

--- src/app/App.js
+++ src/app/App.js
@@ -1,16 +1,14 @@
 import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import RoutesContainer from "../components/routes/RoutesContainer";
-import NavBarContainer from "../components/containers/NavBarContainer";
-import LoginContainer from "../components/containers/LoginContainer";
+import {LoginContainer} from "../components/containers"
+
 class App extends Component {
   render() {
     return (
       <div className="app">
         <header className="app-header">
-          <RoutesContainer />
-          <NavBarContainer />
-          <LoginContainer />
+          <LoginContainer/>
+          
         </header>
       </div>
     );
--- src/components/views/index.js
+++ src/components/views/index.js
@@ -1,3 +1,6 @@
 // Barrel file for presentational components, which will be used to compose our smart containers;
 export { default as MealPlanView } from './MealPlanView';
-export { default as NavBarViewer } from "./NavBarViewer";
+export { default as NavBarView } from "./NavBarView";
+export {default as LoginView } from "./LoginView";
+
+
