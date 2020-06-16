9d42d345274980843cfa59d5febdbcee529166c1
Omaru Jawara <omaru.jawara94@gmail.com>
2020-06-16 16:38:23 -0400

    registration page created

--- src/app/App.js
+++ src/app/App.js
@@ -1,15 +1,13 @@
 import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-
-import {NavBarContainer} from "../components/containers/"
+import {RegisterContainer} from "../components/containers"
 
 class App extends Component {
   render() {
     return (
       <div className="app">
         <header className="app-header">
-          < NavBarContainer/>
-
+          <RegisterContainer />
         </header>
       </div>
     );
--- src/components/views/index.js
+++ src/components/views/index.js
@@ -1,3 +1,3 @@
 // Barrel file for presentational components, which will be used to compose our smart containers;
 // export { default as MealPlanView } from './MealPlanView';
-export { default as NavBarView } from "./NavBarView";
+export { default  as RegisterView} from "./RegisterView" 
