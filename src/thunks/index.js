35b33d28b58e4b2fd686cb55a52d47ab7d869e44
BuildTools <justinkrasner98@gmail.com>
2020-06-22 00:14:51 -0400

    added base for exercise functionality

--- src/thunks/index.js
+++ src/thunks/index.js
@@ -1,3 +1,4 @@
 // Barrel file for thunks, which will be mapped to and invoked within our smart containers;
 export * from '../store/utilities/food';
 export * from '../store/utilities/users';
+export * from '../store/utilities/exercises';
