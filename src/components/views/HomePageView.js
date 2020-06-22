aba7a24e34d54a520e8f600a6670e5908e8cb630
Ivan Lei <ilei0893@gmail.com>
2020-06-22 18:52:51 -0400

    Added Homepage styling

--- src/components/views/HomePageView.js
+++ src/components/views/HomePageView.js
@@ -8,7 +8,6 @@ import { MDBMask, MDBContainer,MDBView } from "mdbreact";
 
 const HomePageView = (props) => {
   return (
-    <div className="homepage">
       <div className="bg">
         {/* <Container className="text">
           <Row className="justify-content-md-center">
@@ -23,36 +22,20 @@ const HomePageView = (props) => {
             </h5>
           </Row>
         </Container> */}
-      </div>
-      
-      <div className="contianer">
-          <Jumbotron>
-          <h1 class="display-3 font-bold white-text mb-2">
+          {/* <h1 className="text">
               Welcome to Fitness Helper
             </h1>
+            <hr class="hr-light"></hr>
             <h5>
               A simple app where you can track food, nutrition, and exercises! A
               simple app where you can track food, nutrition, and exercises! A
               simple app where you can track food, nutrition, and exercises! A
               simple app where you can track food, nutrition, and exercises!
-            </h5>
-          </Jumbotron>
-        </div>
-    </div>
-    
-    
-
-    //   {/* <div id="intro" class="view">
-    //     <div class="full-bg-img">
-
-    //     </div>
-    //     <div >
-    //     <h1>Welcome to Fitness Helper</h1>
-    //     <p>
-    //       bruh bruh bruh bruh bruh bruh bruh bruh bruh bruh bruh bruh bruh bruh
-    //       bruh bruh bruh bruh bruh bruh
-    //     </p>
-    //   </div>
+            </h5> */}
+            <div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '100vh'}} className="text">
+                <h1> Welcome to FitnessHelper </h1>
+            </div>
+          </div>
   );
 };
 
